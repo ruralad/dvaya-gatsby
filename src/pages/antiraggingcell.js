fe540a69dee53649b171a2ce6886b1bad7c9789b
Abhinav Lakshmanan <46086523+ruralad@users.noreply.github.com>
2021-04-01 22:08:22 +0530

    Initial Commit

---
+++ src/pages/antiraggingcell.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+
+import Navbar from "../../components/navbar";
+
+
+function Index() {
+  return (
+    <main>
+      <Helmet>
+        <title>Anti-Ragging Cell | Dvaya</title>
+      </Helmet>
+      <Navbar />
+
+    </main>
+  );
+}
+
+export default Index;
