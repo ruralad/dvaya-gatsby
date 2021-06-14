fb2e1edcb549798e7b10fc2ce09e02a921495980
Abhinav Lakshmanan <46086523+ruralad@users.noreply.github.com>
2021-06-14 16:09:23 +0530

    Style Update

--- src/pages/bloodbank.js
+++ src/pages/bloodbank.js
@@ -81,119 +81,121 @@ class Bloodbank extends React.Component {
           <title>Bloodbank | Dvaya</title>
         </Helmet>
         <Navbar textcolor="black" />
-        <div className={style.container}>
-          <div className={style.box1}>
-            <div className={style.mini_box}>SFI GECI</div>
-            <h1 className={style.heading}>Bloodbank Registration</h1>
-            <div
-            className={style.registering}
-            style={{
-              display:
-                this.state.finalise && !this.state.success ? "block" : "none",
-            }}
-          >
-            submitting...
-          </div>
-          <div
-            className={style.success}
-            style={{
-              display: this.state.success ? "block" : "none",
-            }}
-          >
-            Success✅! Thank you for being part of this campaign.
-          </div>
-          </div>
-          <form
-            className={style.form}
-            onSubmit={this.handleSubmit}
-            style={{ display: this.state.showNext ? "none" : "flex" }}
-          >
-            <p className={style.para}>enter your name and mobile number</p>
-            <input
-              type="text"
-              required
-              className={style.input}
-              placeholder="Full Name"
-              onChange={this.handleChange}
-            />
-            <input
-              type="text"
-              className={style.input}
-              placeholder="Mobile Number"
-              required
-              onChange={this.handleMobile}
-            />
-            <input className={style.submit} type="submit" />
-          </form>
-
-          <form
-            className={style.form}
-            onSubmit={this.handleFinalSubmit}
-            style={{
-              display:
-                this.state.showNext && !this.state.finalise ? "flex" : "none",
-            }}
-          >
-            <p className={style.para}>
-              hi {this.state.name}, please fill the details to complete the
-              registration
-            </p>
-            <div className={style.label}>Department</div>
-
-            <select placeholder={this.state.dept} onChange={this.handleDept}>
-              <option value="CSE">CSE</option>
-              <option value="EC">EC</option>
-              <option value="ME">ME</option>
-              <option value="IT">IT</option>
-              <option value="EEE">EEE</option>
-            </select>
-
-            <div className={style.label}>Semester</div>
-
-            <select
-              placeholder={this.state.semester}
-              onChange={this.handleSemester}
+        <div className={style.hero}>
+          <div className={style.container}>
+            <div className={style.box1}>
+              <div className={style.mini_box}>SFI GECI</div>
+              <h1 className={style.heading}>Bloodbank Registration</h1>
+              <div
+                className={style.success}
+                style={{
+                  display:
+                    this.state.finalise && !this.state.success
+                      ? "block"
+                      : "none",
+                }}
+              >
+                submitting...
+              </div>
+              <div
+                className={style.success}
+                style={{
+                  display: this.state.success ? "block" : "none",
+                }}
+              >
+                Success✅! Thank you for being part of this campaign.
+              </div>
+            </div>
+            <form
+              className={style.form}
+              onSubmit={this.handleSubmit}
+              style={{ display: this.state.showNext ? "none" : "flex" }}
             >
-              <option value="S2">S2</option>
-              <option value="S4">S4</option>
-              <option value="S6">S6</option>
-              <option value="S8">S8</option>
-            </select>
+              <p className={style.para}>enter your name and mobile number</p>
+              <input
+                type="text"
+                required
+                className={style.input}
+                placeholder="Full Name"
+                onChange={this.handleChange}
+              />
+              <input
+                type="text"
+                className={style.input}
+                placeholder="Mobile Number"
+                required
+                onChange={this.handleMobile}
+              />
+              <input className={style.submit} type="submit" />
+            </form>
 
-            <div className={style.label}>District</div>
-            <select
-              placeholder={this.state.district}
-              onChange={this.handleDistrict}
+            <form
+              className={style.form}
+              onSubmit={this.handleFinalSubmit}
+              style={{
+                display:
+                  this.state.showNext && !this.state.finalise ? "flex" : "none",
+              }}
             >
-              <option value="Kasargod">Kasargod</option>
-              <option value="Kannur">Kannur</option>
-              <option value="Wayanad">Wayanad</option>
-              <option value="Kozhikode">Kozhikode</option>
-              <option value="Malappuram">Malappuram</option>
-              <option value="Palakkad">Palakkad</option>
-              <option value="Thrissur">Thrissur</option>
-              <option value="Ernakulam">Ernakulam</option>
-              <option value="Idukki">Idukki</option>
-              <option value="Kottayam">Kottayam</option>
-              <option value="Pathanamthitta">Pathanamthitta</option>
-              <option value="Alappuzha">Alappuzha</option>
-              <option value="Kollam">Kollam</option>
-              <option value="Trivandrum">Trivandrum</option>
-            </select>
-            <div className={style.label}>Blood Group</div>
+              <p className={style.para}>
+                hi {this.state.name}, please fill the details to complete the
+                registration
+              </p>
+              <div className={style.label}>Department</div>
 
-            <input
-              className={style.input2}
-              type="text"
-              placeholder="eg:A+ve,AB-ve..."
-              onChange={this.handleGroup}
-              required
-            />
+              <select placeholder={this.state.dept} onChange={this.handleDept}>
+                <option value="CSE">CSE</option>
+                <option value="EC">EC</option>
+                <option value="ME">ME</option>
+                <option value="IT">IT</option>
+                <option value="EEE">EEE</option>
+              </select>
 
-            <input className={style.submit} type="submit" />
-          </form>
+              <div className={style.label}>Semester</div>
 
-          
+              <select
+                placeholder={this.state.semester}
+                onChange={this.handleSemester}
+              >
+                <option value="S2">S2</option>
+                <option value="S4">S4</option>
+                <option value="S6">S6</option>
+                <option value="S8">S8</option>
+              </select>
+
+              <div className={style.label}>District</div>
+              <select
+                placeholder={this.state.district}
+                onChange={this.handleDistrict}
+              >
+                <option value="Kasargod">Kasargod</option>
+                <option value="Kannur">Kannur</option>
+                <option value="Wayanad">Wayanad</option>
+                <option value="Kozhikode">Kozhikode</option>
+                <option value="Malappuram">Malappuram</option>
+                <option value="Palakkad">Palakkad</option>
+                <option value="Thrissur">Thrissur</option>
+                <option value="Ernakulam">Ernakulam</option>
+                <option value="Idukki">Idukki</option>
+                <option value="Kottayam">Kottayam</option>
+                <option value="Pathanamthitta">Pathanamthitta</option>
+                <option value="Alappuzha">Alappuzha</option>
+                <option value="Kollam">Kollam</option>
+                <option value="Trivandrum">Trivandrum</option>
+              </select>
+              <div className={style.label}>Blood Group</div>
+
+              <input
+                className={style.input2}
+                type="text"
+                placeholder="eg:A+ve,AB-ve..."
+                onChange={this.handleGroup}
+                required
+              />
+
+              <input className={style.submit} type="submit" />
+            </form>
+          </div>
         </div>
       </main>
     );
